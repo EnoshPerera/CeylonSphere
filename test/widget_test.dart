b9d54e222c22207d230ec22dc5e1fdfadb4befbf
Chamindu Vimeth <vimethc@gmail.com>
2025-03-23 04:46:57 +0530

    chanaged file paths

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:newshop/main.dart';
+import 'package:newshop/Souvenir_Shops/shop_main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
